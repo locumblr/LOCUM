4eb64555fc08d9892ee6fedc49d81457d47ad825
Dr. Akhil Xavier Joseph <dr.akhilxavierjoseph@Akhil-Xavier-Joseph-2.local>
2026-05-10 17:36:22 +0530

    Add nurse dashboard, locums and admin panel update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l7b0qcc0p9g"
+    "revision": "0.eivrdg595h4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
